5f2d63b1d8676b5793fe88281264cc6cd7bc75cd
adilzhanY <adilzhanit@gmail.com>
2025-08-31 01:24:16 +0200

    appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,6 @@
+
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.adilier.foodapp",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
