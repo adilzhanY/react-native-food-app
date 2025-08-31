3d1bcf0e12810fd6a67d431f8214691a24559296
adilzhanY <adilzhanit@gmail.com>
2025-08-31 02:38:04 +0200

    feat: Implement auth screens

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -3,4 +3,7 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.adilier.foodapp",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "68b38899003824a7c059",
+  userCollectionId: 'user',
+  
 }
